546677faa194152ac27dcf51db04b00f9a8e79ce
Chillup <philipkaim@gmail.com>
2018-09-08 18:57:34 -0400

    connect and disconnect event handelelers

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const app = express();
 const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User was disconected');
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });

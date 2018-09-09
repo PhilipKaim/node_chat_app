b07c06945988ddc9f6cab2c7759d83609d2512bf
Chillup <philipkaim@gmail.com>
2018-09-09 11:03:28 -0400

    fixed missing js file

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+const socket = io();
+
+socket.on('connect', () => {
+    console.log('connected to server');
+});
+
+socket.on('disconnect', () => {
+    console.log('disconected from server');
+});
+
+socket.on('newMessage', (message) => {
+    console.log('new message:', message);
+});
+

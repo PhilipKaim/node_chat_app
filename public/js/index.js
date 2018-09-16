f3c3ec08e1bf8fc9eed63cffa611b127f378224f
Chillup <philipkaim@gmail.com>
2018-09-16 10:56:37 -0400

    scroll user to bottom of chat

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 const socket = io();
-const $ = jQuery; 
+const $ = jQuery;
+
+const scrollToBottom = () => {
+    // Selectors
+    const messages = $('#messages');
+    const newMessage = messages.children('li:last-child');
+    // Heights
+    const clientHeight = messages.prop('clientHeight');
+    const scrollTop = messages.prop('scrollTop');
+    const scrollHeight = messages.prop('scrollHeight');
+    const newMessageHeight = newMessage.innerHeight();
+    const lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+};
 
 socket.on('connect', () => {
     console.log('connected to server');
@@ -19,6 +35,7 @@ socket.on('newMessage', (message) => {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', (message) => {
@@ -31,6 +48,7 @@ socket.on('newLocationMessage', (message) => {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 $('#message-form').on('submit', (e) => {

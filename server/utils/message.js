2ba115b1c019b659b4fb772cb1b8910bb02d1f62
Chillup <philipkaim@gmail.com>
2018-09-22 10:55:19 -0400

    sending messages to users room only

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,19 +1,19 @@
-const moment = require('moment');
+var moment = require('moment');
 
-const generateMessage = (from, text) => {
-    return {
-        from,
-        text,
-        createdAt: moment().valueOf()
-    };
-}
-;
-const generateLocationMessage = (from, latitude, longitude) => {
-    return {
-        from,
-        url: `https://www.google.com/maps?q=${latitude},${longitude}`,
-        createdAt: moment().valueOf()
-    };
+var generateMessage = (from, text) => {
+  return {
+    from,
+    text,
+    createdAt: moment().valueOf()
+  };
 };
 
-module.exports = { generateMessage, generateLocationMessage };
+var generateLocationMessage = (from, latitude, longitude) => {
+  return {
+    from,
+    url: `https://www.google.com/maps?q=${latitude},${longitude}`,
+    createdAt: moment().valueOf()
+  };
+};
+
+module.exports = {generateMessage, generateLocationMessage};
